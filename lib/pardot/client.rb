c5d87bd7107ba5ce985ffcd0d09a38f02000a2bd
Bobby Chan <chan.bobby1@gmail.com>
2014-02-17 23:57:12 -0500

    Email send

--- lib/pardot/client.rb
+++ lib/pardot/client.rb
@@ -1,7 +1,7 @@
 module Pardot
-  
+
   class Client
-    
+
     include HTTParty
     base_uri 'https://pi.pardot.com'
 
@@ -15,17 +15,18 @@ module Pardot
     include Objects::Visitors
     include Objects::Visits
     include Objects::VisitorActivities
-    
+    include Objects::Emailer
+
     attr_accessor :email, :password, :user_key, :api_key, :format
-    
+
     def initialize email, password, user_key
       @email = email
       @password = password
       @user_key = user_key
-      
+
       @format = "simple"
     end
-    
-    
+
+
   end
 end
---
+++ lib/pardot/objects/emailer.rb
@@ -0,0 +1,46 @@
+module Pardot
+  module Objects
+    module Emailer
+
+      def emailer
+        @emailer ||= Emailer.new self
+      end
+
+      class Emailer
+
+        def initialize client
+          @client = client
+        end
+
+        def send_by_id id, params = {}
+          post "/do/send/prospect_id/#{id}", params
+        end
+
+        def send_by_email email, params = {}
+          post "/do/send/prospect_email/#{email}", params
+        end
+
+        def send_by_list_ids campaign_id, list_ids, params
+          post "do/send/", params.merge({
+            :campaign_id => campaign_id,
+            :list_ids => list_ids
+          })
+        end
+
+        protected
+
+        def get path, params = {}, result = "email"
+          response = @client.get "email", path, params
+          result ? response[result] : response
+        end
+
+        def post path, params = {}, result = "email"
+          response = @client.post "email", path, params
+          result ? response[result] : response
+        end
+
+      end
+
+    end
+  end
+end
