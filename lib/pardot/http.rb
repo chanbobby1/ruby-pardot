081ec0dd0a4ff223ffdb57bea9c0a3495bf2cbd9
Bobby Chan <chan.bobby1@gmail.com>
2014-02-24 14:27:29 -0500

    Handle json error responses

--- lib/pardot/http.rb
+++ lib/pardot/http.rb
@@ -1,64 +1,67 @@
 module Pardot
   module Http
-    
+
     def get object, path, params = {}, num_retries = 0
       smooth_params object, params
       full_path = fullpath object, path
       check_response self.class.get(full_path, :query => params)
-      
+
     rescue Pardot::ExpiredApiKeyError => e
       handle_expired_api_key :get, object, path, params, num_retries, e
-      
+
     rescue SocketError, Interrupt, EOFError, SystemCallError => e
       raise Pardot::NetError.new(e)
     end
-    
+
     def post object, path, params = {}, num_retries = 0
       smooth_params object, params
       full_path = fullpath object, path
       check_response self.class.post(full_path, :body => params)
-      
+
     rescue Pardot::ExpiredApiKeyError => e
       handle_expired_api_key :post, object, path, params, num_retries, e
-      
+
     rescue SocketError, Interrupt, EOFError, SystemCallError => e
       raise Pardot::NetError.new(e)
     end
-    
+
     protected
-    
+
     def handle_expired_api_key method, object, path, params, num_retries, e
       raise e unless num_retries == 0
-      
+
       reauthenticate
-      
+
       send(method, object, path, params, 1)
     end
-    
+
     def smooth_params object, params
       return if object == "login"
-      
+
       authenticate unless authenticated?
       params.merge! :user_key => @user_key, :api_key => @api_key, :format => 'json', :output => @format, :options => { :headers => { 'ContentType' => 'application/json' } }
     end
-    
+
     def check_response http_response
-      rsp = http_response["rsp"] if http_response.content_type == 'text/xml'
-      rsp = rsp || http_response || {}
-      error = rsp["err"]
-      status = rsp["@attributes"].try(:[],"stat") || rsp["stat"]
-      error ||= "Unknown Failure: #{rsp.inspect}" if status == "fail"
-      content = error['__content__'] if error.is_a?(Hash)
-      
-      if [error, content].include?("Invalid API key or user key") && @api_key
-        raise ExpiredApiKeyError.new @api_key
+      rsp = (http_response.content_type == 'text/xml' ? http_response["rsp"] : http_response) || {}
+
+      if rsp["err"]
+        response_error = {
+          '__content__' => rsp["err"].try(:[], "__content__") || rsp["err"],
+          'code' => rsp["err"].try(:[], "code") || rsp["@attributes"].try(:[], "err_code"),
+          'status' => rsp["stat"] || rsp["@attributes"].try(:[], "stat")
+        }
+
+        if response_error['__content__'] == "Invalid API key or user key" && @api_key
+          raise ExpiredApiKeyError.new @api_key
+        else
+          raise ResponseError.new response_error
+        end
       end
-      
-      raise ResponseError.new error if error
 
       rsp
     end
-    
+
     def fullpath object, path, version = 3
       full = File.join("/api", object, "version", version.to_s)
       unless path.nil?
@@ -66,6 +69,6 @@ module Pardot
       end
       full
     end
-    
+
   end
 end
